44cc0b712efd7d71ffd8a274f00554244a7d3fe9
SamDaSkript <35204174+SamDaSkript@users.noreply.github.com>
2018-09-25 23:22:44 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 import asyncio
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='#')
 
 @bot.event
 async def on_ready():
@@ -21,7 +21,7 @@ async def wave(ctx):
     
 @bot.command()
 async def invite(ctx):
-    await ctx.send("Bot Invite: + "https://discordapp.com/oauth2/authorize?client_id=493870175356190724&scope=bot"")
+    await ctx.send("Bot Invite: https://discordapp.com/oauth2/authorize?client_id=493870175356190724&scope=bot")
     
     
 bot.run('NDkzODcwMTc1MzU2MTkwNzI0.Dox77A.ZYZ5ZUJra6YVVy0cYOzM53r2QhM')
