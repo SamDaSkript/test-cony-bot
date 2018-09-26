38f46298e8d08f5d394a67302ed6af789ce0f661
SamDaSkript <35204174+SamDaSkript@users.noreply.github.com>
2018-09-25 22:44:20 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='/')
 
 @bot.event
 async def on_ready():
@@ -14,18 +14,18 @@ async def on_ready():
 async def greet(ctx):
     await ctx.send(":smiley: :wave: Hello, there!")
 
-# @bot.command()
-# async def help(ctx):
-#     await ctx.send(==========================================)
-#     await cfx.send(Discord Commands:)
-#     await cfx.send(1. /reload (Reload the server and discord bot!)
-#     await cfx.send(2. /help (Help with the bot!)
-#     await cfx.send(3. /inivte (Give's you the discord invite!)
-#     await cfx.send(4. /creator (Tell's you the creator's of this discord and me!)
-#     await cfx.send(==========================================)
+@bot.command()
+async def help(ctx):
+    await ctx.send("==========================================")
+    await cfx.send("Discord Commands:)
+    await cfx.send("1. /reload (Reload the server and discord bot!")
+    await cfx.send("2. /help (Help with the bot!)
+    await cfx.send("3. /inivte (Give's you the discord invite!")
+    await cfx.send("4. /creator (Tell's you the creator's of this discord and me!")
+    await cfx.send("==========================================")
     
 @bot.command()
 async def wave(ctx):
-    await ctx.send(":wave: {0.author.mention}")
+    await ctx.send(":wave:")
     
 bot.run('NDkzODcwMTc1MzU2MTkwNzI0.Dox77A.ZYZ5ZUJra6YVVy0cYOzM53r2QhM')
