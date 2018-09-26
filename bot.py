9ff487df6257a44ffbd65119bd237ab24c297eb4
SamDaSkript <35204174+SamDaSkript@users.noreply.github.com>
2018-09-25 23:23:37 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 import asyncio
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='#')
+bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
